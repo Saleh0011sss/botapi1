76abd224d0b35f9953857cb9ae0dd0dcad6dea10
Jorge Ferreiro <me@jgferreiro.com>
2016-02-20 17:42:34 +0100

    Some documentation + correcting type checking [2/2]

--- userRepository.py
+++ userRepository.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import run, post, get
-import pymongo
-from pymongo import MongoClient, ReturnDocument
-from passlib.hash import pbkdf2_sha256
-import lepl.apps.rfc3696
 import random
+import pymongo
+import lepl.apps.rfc3696
+from bottle import run, post, get
+from passlib.hash import pbkdf2_sha256
+from pymongo import MongoClient, ReturnDocument
 
 dbPath = 'localhost'
 client = MongoClient(dbPath)
@@ -31,8 +31,17 @@ def validPassword(password, hash):
     except: # The password is not well formated (eg, using pbkd2_sha256 algorithm)
         return False
 
-# PUBLIC API METHODS
+def isTypeOf(_elemn, typeStr):
+    if typeStr == "String":
+        return isinstance(_elemn, str)
+    elif typeStr == "Number":
+        return isinstance(_elemn, num)
+    return False 
 
+"""
+Return False if the user exists or some parameters are not valid
+and True if the user was inserted correctly.
+"""
 def createUser(chatID, email, password, phone):
     if not emailValidator(email):
         return False
@@ -57,16 +66,33 @@ Returns Dictionary if the user was found
 and None in the rest of cases. 
 """
 def getUserByChat(chatID):
-    if not isinstance(chatID, str):
+    if not isTypeOf(chatID, 'String'):
         return None # not valid input type
     return db.users.find_one({ '_id' : chatID });
 
+"""
+Returns true if the user does not exist on our system
+and True in the other case
+"""
 def doesUserExist(chatID):
-    return isinstance(chatID, str) and db.users.find_one({ '_id' : chatID }) != None
+    return isTypeOf(chatID, 'String') and db.users.find_one({ '_id' : chatID }) != None
 
+"""
+Given chatID, cardNumber, expDate and CVV we store it into the user.
+Returns True if was successfully inserted
+False in other cases (eg, duplicated card number)
+"""
 def insertPayment(chatID, cardNumber, expDate, CVV):
     pass
 
-print createUser('12031', 'jgferreiro.me@gmail.com', 1231, 696996)
-print getUserByChat('12031')
-print doesUserExist('12031')
+print "Created user: "
+print createUser('122031', 'jgferreiro.me@gmail.com', 1231, 696996)
+
+print "User chat: "
+print getUserByChat('0012031')
+
+print "Does user exist?: "
+print doesUserExist('0012031')
+
+print "Inserted Payment: "
+print insertPayment('12031', '3949439439403', '12/20', '300')
