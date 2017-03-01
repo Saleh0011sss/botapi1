519eac063bb8c7af9975128815f25e1a6e1a54d4
Saleh0011 <salehfffaaa@gmail.com>
2017-03-01 21:29:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import bot
 
-API_TOKEN = os.environ['TELEGRAM_TOKEN']
+API_TOKEN = os.environ['338961451:AAGEdm7OSG4OATpk1EuL7iaGhpIqqSegjzU338961451:AAGEdm7OSG4OATpk1EuL7iaGhpIqqSegjzU']
 
 bot.start(API_TOKEN)
